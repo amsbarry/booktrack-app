f8b0e7bb7dce10c185b2a7fff5f40d3d2448ae43
Checku Barra <checkub@gmail.com>
2025-08-14 20:31:28 +0000

    feat: Fetch and display list of books from API.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import BookList from './components/BookList';
 function App() {
+    const [books, setBooks] = useState([]);
+    useEffect(() => {
+          fetch('http://localhost:3001/books')
+            .then(response => response.json())
+            .then(data => setBooks(data))
+            .catch(error => console.error("Error fetching data:", error));
+        }, []);
+        
   return (
     <div className="App">
-      <h1>BookTrack</h1>
+      <BookList books={books} />
     </div>
   );
 }
